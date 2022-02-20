930ea75d6b4efefdbebb41823048213058d935df
malvadobrr <96801046+malvadobrr@users.noreply.github.com>
2022-02-20 14:35:20 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
 <?php
-
-/**
- * ====================================================================================
- *                           Google Drive Proxy Player (c) CodySeller
- * ----------------------------------------------------------------------------------
- * @copyright This software is exclusively sold at codyseller.com. If you have downloaded this
- *  from another site or received it from someone else than me, then you are engaged
- *  in an illegal activity. You must delete this software immediately or buy a proper
- *  license from https://codyseller.com/license.
- *
- *  Thank you for your cooperation and don't hesitate to contact me if anything :)
- * ====================================================================================
- *
- * @author CodySeller (http://codyseller.com)
- * @link http://codyseller.com
- * @license http://codyseller.com/license
- */
-
-
-
-if(!file_exists("includes/config.php")){
-  header("Location: install");
-  exit;
-}
-
-require_once(__DIR__.'/includes/config.php');
-
-$app->run();
+header( 'Content-Type: text/html' );
+require_once( __DIR__ . '/drivexceed.setup.php' );
+require_once( __DIR__ . '/drivexceed.configuration.php' );
+$setup = json_encode( $configuration );
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title><?php
+echo $title;
+?></title>
+		<script type="text/javascript" src="./resources/js/jwpatch.min.js"></script>
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<style type="text/css">
+		html, body {
+			margin: 0;
+			padding: 0;
+		}
+		#drivexceed-container {
+			position: absolute;
+			width: 100% !important;
+			height: 100% !important;
+		}
+		</style>
+	</head>
+	<body>
+		<div id="drivexceed-container"></div>
+		<script type="text/javascript">
+		window.drivexceed = function() {
+			window.jwplayer.key = 'https://github.com/masgasatriawirawan/drivexceed/';
+			window.jwplayer( 'drivexceed-container' ).setup( <?php echo $setup; ?> );
+		};
+		</script>
+	</body>
+</html>
+<?php
+exit;
+?>
